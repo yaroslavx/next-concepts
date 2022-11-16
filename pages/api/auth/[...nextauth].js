1ca7684833a30bd83810c176f5773fea55a1a924
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-11-16 15:39:51 +0300

    add next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,4 +8,22 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    secret: 'someRandomSecret',
+  },
+  callbacks: {
+    async jwt(token, user) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session(session, token) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 });
